2a340decdfcf252eceac60514f4b57927cb96210
josip684 <goreta684@gmail.com>
2020-08-17 23:12:45 +0200

    adding Base Entity. Closes #10

--- pet-clinic-data/src/main/java/com/example/jgpetclinicproject/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/jgpetclinicproject/model/Person.java
@@ -1,6 +1,6 @@
 package com.example.jgpetclinicproject.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstname;
     private String lastname;
 
--- pet-clinic-data/src/main/java/com/example/jgpetclinicproject/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/jgpetclinicproject/model/PetType.java
@@ -1,6 +1,6 @@
 package com.example.jgpetclinicproject.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
