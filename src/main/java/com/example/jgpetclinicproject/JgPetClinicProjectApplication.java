06058ca1531c4ce4c303fa37954582be1aa602e3
josip684 <goreta684@gmail.com>
2020-08-17 14:04:14 +0200

    Initial Project Add

---
+++ src/main/java/com/example/jgpetclinicproject/JgPetClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jgpetclinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgPetClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JgPetClinicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jgpetclinicproject/JgPetClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jgpetclinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgPetClinicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
