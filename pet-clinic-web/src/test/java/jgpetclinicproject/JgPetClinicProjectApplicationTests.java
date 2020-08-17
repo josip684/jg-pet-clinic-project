2c18103638a39da51beb74c5496d9f6e2a2dd0c4
josip684 <goreta684@gmail.com>
2020-08-17 16:58:21 +0200

    refactoring to multimodule maven project. Closes #1.

--- pet-clinic-web/src/test/java/jgpetclinicproject/JgPetClinicProjectApplicationTests.java
+++ pet-clinic-web/src/test/java/jgpetclinicproject/JgPetClinicProjectApplicationTests.java
@@ -1,9 +1,10 @@
-package com.example.jgpetclinicproject;
+package jgpetclinicproject;
 
+import com.example.jgpetclinicproject.JgPetClinicProjectApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JgPetClinicProjectApplication.class)
 class JgPetClinicProjectApplicationTests {
 
     @Test
