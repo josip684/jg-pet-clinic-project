7cb869ab8c24af8e8e72e3282a774e09a1f16daa
josip684 <goreta684@gmail.com>
2020-08-20 14:05:59 +0200

    added spring data JPA service for Specialty. Closes #44

--- pet-clinic-web/src/main/java/com/example/jgpetclinicproject/JgPetClinicProjectApplication.java
+++ pet-clinic-web/src/main/java/com/example/jgpetclinicproject/JgPetClinicProjectApplication.java
@@ -1,12 +1,12 @@
 package com.example.jgpetclinicproject;
 
-import com.example.jgpetclinicproject.services.map.OwnerServiceMap;
+import com.example.jgpetclinicproject.services.map.OwnerMapService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@EnableJpaRepositories(basePackageClasses = {OwnerSDJpaService.class})
-@EnableJpaRepositories(basePackageClasses = {OwnerServiceMap.class})
+@EnableJpaRepositories(basePackageClasses = {OwnerMapService.class})
 @SpringBootApplication
 public class JgPetClinicProjectApplication {
 
